1fb8731a66611891d5ba6affc5c337b6ffe1220f
Osiaro Famous <famous.osiaro@gmail.com>
2022-09-08 15:33:33 +0000

    task 0-6 on 0x00-hello_world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <studio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return; Always 0 (Sucess)
+ */
+int main(void)
+{
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+
+printf("size of a char; %lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("size of an int; %lu bytes(s)\n", (unsigned long)sizeof(a));
+printf("size of a long int; %lu bytes(s)\n", (unsigned long)sizeof(b));
+printf("size of a long long int; %lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("size of a float; %lu bytes(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
