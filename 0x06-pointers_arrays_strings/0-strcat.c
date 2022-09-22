75da29aeb088c192904ccd7128fb975df7ed2108
Osiaro Famous <famous.osiaro@gmail.com>
2022-09-22 11:05:12 +0000

    task 0 on 0x06-pointers_arrays_strings$

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two string together pointed to by @src
- * null byte, to the end of the string pointed to by @dest
- * @src: The source string to be appended to @dest
- * @dest: A pointer to the string to be concatenated upon
- *
- * Return: A pointer to the destination string @dest
+ * _strcat - concatenates two string together
+ * @src: Char[] to append to dest
+ * @dest: Char[] to be appended to
+ * Return: Always 0.
  */
 
 char *_strcat(char *dest, char *src)
